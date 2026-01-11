f6dfbf2ed002d406006fa6cafb70da4b1bc96e93
William Odhiambo <williamsisulu2003@gmail.com>
2026-01-11 12:35:05 +0300

    Prebuild

--- asiyowc/server.js
+++ asiyowc/server.js
@@ -1,3 +1,5 @@
 //BACKEND SERVER ENDPOINT
 
-export const server = "http://192.168.1.112:5000/api";
+// export const server = "http://192.168.1.112:5000/api";
+
+export const server = "https://asiyowc.onrender.com/api"; 
